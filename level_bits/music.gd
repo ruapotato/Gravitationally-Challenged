a5648b309808ec0ca00fbf69b2053eb97f3afcfb
David Hamner <ke7oxh@gmail.com>
2025-02-16 19:56:38 -0800

    Better music

--- level_bits/music.gd
+++ level_bits/music.gd
@@ -5,9 +5,10 @@ var level_loader
 var player
 var music_score = {
 	"hub": preload("res://AI_GEN/music/Chiptune-folk fusion - Game time.wav"),
-	"level1": preload("res://AI_GEN/music/Chiptune-folk fusion - Game time.wav"),
+	"level1": preload("res://AI_GEN/music/level1.wav"),
 	"level3": preload("res://AI_GEN/music/Chiptune-folk fusion - Game time.wav"),
-	"level2": preload("res://AI_GEN/music/end_song.wav"),
+	"boss1": preload("res://AI_GEN/music/boss1.wav"),
+	"end": preload("res://AI_GEN/music/end_song.wav"),
 }
 
 # Called when the node enters the scene tree for the first time.
